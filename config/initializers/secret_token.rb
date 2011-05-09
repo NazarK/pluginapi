700d1324e6a75699f903d3a2da41b5791b7dbcea
nazar kuliev <nazar.kuliev@gmail.com>
2011-05-09 22:59:41 +0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pluginapi::Application.config.secret_token = '13192363e86e12add6c7604961201312f178df35ddbb636209466612702ec166041062cfa2e3bd5b8fa131eafbad7146848cecae5a63a7b7cf938499818ad447'
